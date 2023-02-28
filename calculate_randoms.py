efda9280fb8559d071454dafbf8e8c479c9ec83c
WilliamsJakeL <williamsjl@uchicago.edu>
2023-02-28 14:03:25 -0600

    Change BhattKernel to use multiple values of t and test this on random graphs along with fully randomly generated graphs

--- calculate_randoms.py
+++ calculate_randoms.py
@@ -43,18 +43,16 @@ test_y = [.25 for _ in range(100)] + \
           [.75 for _ in range(100)]
 test_y_np = np.array(test_y)
 
-for r in [0.0001, 0.1, 100, 10000]:
-    print("Lambda =",r)
-    K = BhattKernel(t=0.5, num_bins=40, graphs=train_graphs, y=train_y,r_lambda=r,calcWeights=True)
+# K = BhattKernel(t=[0.5], num_bins=40, graphs=train_graphs, y=train_y,r_lambda=100,calcWeights=True)
 
-    preds = np.array([K.predictGraph(g) for g in test_graphs])
+# preds = np.array([K.predictGraph(g) for g in test_graphs])
 
-    print("Average absolute error:", np.mean(np.abs(preds - test_y_np)))
-    print("Average absolute error (N=10):", np.mean(np.abs(preds[:300] - test_y_np[:300])))
-    print("Average absolute error (N=25):", np.mean(np.abs(preds[300:600] - test_y_np[300:600])))
-    print("Average absolute error (N=17):", np.mean(np.abs(preds[600:900] - test_y_np[600:900])))
-    print("Average absolute error (N=50):", np.mean(np.abs(preds[900:] - test_y_np[900:])), '\n')
-    # print("Average absolute percent error:", np.mean(np.abs(preds-test_y_np)/test_y_np))
+# print("Average absolute error:", np.mean(np.abs(preds - test_y_np)))
+# print("Average absolute error (N=10):", np.mean(np.abs(preds[:300] - test_y_np[:300])))
+# print("Average absolute error (N=25):", np.mean(np.abs(preds[300:600] - test_y_np[300:600])))
+# print("Average absolute error (N=17):", np.mean(np.abs(preds[600:900] - test_y_np[600:900])))
+# print("Average absolute error (N=50):", np.mean(np.abs(preds[900:] - test_y_np[900:])), '\n')
+# print("Average absolute percent error:", np.mean(np.abs(preds-test_y_np)/test_y_np))
 
 train_graphs_50 = [Graph.generateRandom(N=10, p=0.25) for _ in range(100)] + \
          [Graph.generateRandom(N=10, p=0.5) for _ in range(100)] + \
@@ -77,15 +75,30 @@ train_y_50 = [.25 for _ in range(100)] + \
 
 print("\n Add in N=50 graphs! \n")
 
-for r in [0.0001, 0.1, 100, 10000]:
-    print("Lambda =",r)
-    K = BhattKernel(t=0.5, num_bins=40, graphs=train_graphs_50, y=train_y_50,r_lambda=r,calcWeights=True)
+K = BhattKernel(t=[0.01, 0.1, 0.5, 1.0], num_bins=40, graphs=train_graphs_50, y=train_y_50,r_lambda=100,calcWeights=True)
 
-    preds = np.array([K.predictGraph(g) for g in test_graphs])
+preds = np.array([K.predictGraph(g) for g in test_graphs])
 
-    print("(50) Average absolute error:", np.mean(np.abs(preds - test_y_np)))
-    print("(50) Average absolute error (N=10):", np.mean(np.abs(preds[:300] - test_y_np[:300])))
-    print("(50) Average absolute error (N=25):", np.mean(np.abs(preds[300:600] - test_y_np[300:600])))
-    print("(50) Average absolute error (N=17):", np.mean(np.abs(preds[600:900] - test_y_np[600:900])))
-    print("(50) Average absolute error (N=50):", np.mean(np.abs(preds[900:] - test_y_np[900:])), '\n')
-    # print("Average absolute percent error:", np.mean(np.abs(preds-test_y_np)/test_y_np))
+print("(50) Average absolute error:", np.mean(np.abs(preds - test_y_np)))
+print("(50) Average absolute error (N=10):", np.mean(np.abs(preds[:300] - test_y_np[:300])))
+print("(50) Average absolute error (N=25):", np.mean(np.abs(preds[300:600] - test_y_np[300:600])))
+print("(50) Average absolute error (N=17):", np.mean(np.abs(preds[600:900] - test_y_np[600:900])))
+print("(50) Average absolute error (N=50):", np.mean(np.abs(preds[900:] - test_y_np[900:])), '\n')
+# print("Average absolute percent error:", np.mean(np.abs(preds-test_y_np)/test_y_np))
+
+print("Test Full Random Graph Generation")
+
+r = np.random.random((1000,2))
+train_all_random = [Graph.generateRandom(int(n*98)+2, m) for (n,m) in r]
+train_all_ry = [m for (_, m) in r]
+
+K = BhattKernel(t=[0.01, 0.1, 0.5, 1.0], num_bins=40, graphs=train_all_random, y=train_all_ry,r_lambda=100,calcWeights=True)
+
+preds = np.array([K.predictGraph(g) for g in test_graphs])
+
+print("(50) Average absolute error:", np.mean(np.abs(preds - test_y_np)))
+print("(50) Average absolute error (N=10):", np.mean(np.abs(preds[:300] - test_y_np[:300])))
+print("(50) Average absolute error (N=25):", np.mean(np.abs(preds[300:600] - test_y_np[300:600])))
+print("(50) Average absolute error (N=17):", np.mean(np.abs(preds[600:900] - test_y_np[600:900])))
+print("(50) Average absolute error (N=50):", np.mean(np.abs(preds[900:] - test_y_np[900:])), '\n')
+# print("Average absolute percent error:", np.mean(np.abs(preds-test_y_np)/test_y_np))
--- kernels.py
+++ kernels.py
@@ -6,12 +6,16 @@ class BhattKernel:
 
     def __init__(self, t, num_bins, graphs=None, y=None, r_lambda=0, calcWeights=False):
         self.t = t
+        self.numT = len(t)
         self.num_bins = num_bins
         self.graphs = graphs
         self.y = y
         self.r_lambda = r_lambda
         if not self.graphs is None:
-            self.binnedGraphs = [self.graphBinned(g) for g in self.graphs]
+            if self.numT == 1:
+                self.binnedGraphs = [self.graphBinnedSingle(g) for g in self.graphs]
+            else:
+                self.binnedGraphs = [self.graphBinned(g) for g in self.graphs]
         if not calcWeights:
             self.weights = np.zeros(self.num_bins)
         else:
@@ -21,15 +25,29 @@ class BhattKernel:
     def predictGraph(self, g):
         return self.weights@np.sqrt(self.graphBinned(g))
 
-    def graphBinned(self, g):
+    def graphBinnedSingle(self, g):
         L = g.getLaplacian()
-        h = expm(-(self.t/g.vertN)*L)
+        h = expm(-self.t[0]*L)
         pi = [0 for _ in range(self.num_bins)]
         for v in np.nditer(h):
             if v == 1:
                 pi[self.num_bins-1] += 1
             else:
                 pi[int(v*self.num_bins)] += 1
+        return pi
+
+    def graphBinned(self, g):
+        L = g.getLaplacian()
+        # h = expm(-(self.t/g.vertN)*L)
+        w, v = np.linalg.eigh(L)
+        pi = [0 for _ in range(self.num_bins*self.numT)]
+        for i, B in enumerate(self.t):
+            h = v@np.diag(np.exp(-B*w))@v.T
+            for entry in np.nditer(h):
+                if entry >= 1:
+                    pi[(i*self.num_bins)+self.num_bins-1] += 1
+                else:
+                    pi[(i*self.num_bins)+int(entry*self.num_bins)] += 1
         return pi
 
     def calculateWeights(self):
