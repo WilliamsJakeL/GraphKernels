2e5f014a9d56da3a637fd6586598a3478188f489
WilliamsJakeL <williamsjl@uchicago.edu>
2023-03-07 14:02:50 -0600

    Working on XYZ to mol for CSD

--- dataload_pipeline.py
+++ dataload_pipeline.py
@@ -9,6 +9,11 @@ from tqdm import tqdm
 
 from graph import Graph
 
+import rdkit
+from rdkit import Chem
+from rdkit.Chem import rdDetermineBonds
+from rdkit.Chem.rdmolfiles import MolFromXYZBlock as xyzB
+
 CSD_subsets = {
     'CSD-2k': {
         'filename': 'data/CSD-2k.txt'
@@ -41,14 +46,66 @@ def params_CSD():
         outfiles = td(f"{exp_name}.pickle")
         yield infiles, outfiles, exp_name, exp_config
 
+def params_CSD_rdkit():
+    for exp_name, exp_config in CSD_subsets.items():
+        infiles = exp_config['filename']
+        outfiles = td(f"{exp_name}.rdkit.pickle")
+        yield infiles, outfiles, exp_name, exp_config
+
 def params_PROTEINS():
     for exp_name, exp_config in PROTEINS.items():
         infiles = exp_config['filename']
         outfiles = td(f"{exp_name}.pickle")
         yield infiles, outfiles, exp_name, exp_config
 
+BOND_TYPE_TO_LABEL = {
+    rdkit.Chem.rdchem.BondType.SINGLE : 1,
+    rdkit.Chem.rdchem.BondType.DOUBLE : 2,
+    rdkit.Chem.rdchem.BondType.TRIPLE : 3,
+    rdkit.Chem.rdchem.BondType.AROMATIC : 1.5
+}
+
 def getDistance(x1, y1, z1, x2, y2, z2):
     return np.linalg.norm(np.array([float(x1), float(y1), float(z1)]) - np.array([float(x2), float(y2), float(z2)]))
+
+def load_graph(mol):
+    # mol = Chem.Mol(row['rdmol'])
+    n = mol.GetNumAtoms()
+    edges, efs = [], []
+    for e in mol.GetBonds():
+        edges += [(e.GetBeginAtomIdx()+1, e.GetEndAtomIdx()+1)]
+        efs += [BOND_TYPE_TO_LABEL[e.GetBondType()]]
+    vfs = [a.GetAtomicNum() for a in mol.GetAtoms()]
+    return Graph(n, edges, vfs, efs)
+
+@mkdir(OUTPUT_DIR)
+@files(params_CSD_rdkit)
+def convert_CSD_to_rdkit_graphs(infiles, outfiles, exp_name, exp_config):
+    print("Converting text to graphs:", exp_name)
+    xyz_file = infiles
+    with open(xyz_file) as file:
+        lines = file.readlines()
+    i = 0
+    id = 0
+    d = {}
+    while i < len(lines):
+        n = int(lines[i])
+        shifts = []
+        block = ''
+        block += lines[i]
+        block += lines[i+1]
+        for j in range(n):
+            z = lines[i+j+2].split()
+            block += z[0] + '\t' + z[1] + '\t' + z[2] + '\t' + z[3] + '\n'
+            shifts += [float(z[4])]
+        m = xyzB(block)
+        rdDetermineBonds.DetermineConnectivity(m)
+        g = load_graph(m)
+        d[id] = (g, shifts)
+        id += 1
+        i += n + 2
+    print("Found", len(d), "graphs.")
+    pickle.dump(d, open(outfiles, 'wb'))
 
 @mkdir(OUTPUT_DIR)
 @files(params_CSD)
@@ -116,4 +173,4 @@ def convert_to_graphs(infiles, outfiles, exp_name, exp_config):
     pickle.dump(d, open(outfiles, 'wb'))
 
 if __name__ == "__main__":
-    pipeline_run([convert_CSD_to_graphs, convert_to_graphs], checksum_level=0)
+    pipeline_run([convert_CSD_to_rdkit_graphs, convert_CSD_to_graphs, convert_to_graphs], checksum_level=0)
--- prediction_pipeline.py
+++ prediction_pipeline.py
@@ -78,6 +78,17 @@ EXPERIMENTS_nodes = {
         'nodes_to_predict': [0]
     },
 
+    'CSD_rdkit_H_only': {
+        'train_file': 'data/CSD-2k.rdkit.pickle',
+        'test_file': 'data/CSD-500.rdkit.pickle',
+        't': [0.001, 0.01, 0.1, 1, 10],
+        'num_bins': 40,
+        'r_lambda': 100,
+        'label_types': [1,6,7,8,9,15,16,17],
+        'use_labels': True, 
+        'nodes_to_predict': [1]
+    },
+
     'nmrshiftdb_H_only': {
         'train_file': 'data/nmrshiftdb.1H.train.pickle',
         'test_file': 'data/nmrshiftdb.1H.test.pickle',
